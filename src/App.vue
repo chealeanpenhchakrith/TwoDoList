8359596779ab3ebfd7b892f7e6364d66e1084e04
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-08-07 16:45:24 +0200

    Link the entered task with the button task to save and display accordingly the current task typed

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
-<script setup>
-
-</script>
-
 <template>
   <h1>To Do List</h1>
-  <label> Enter a task : 
-    <input type="text">
+  <label>
+    Enter a task :
+    <input type="text" v-model="currentTask" />
   </label>
-  <button>Add Task</button>
+  <button v-on:click="addTask">Add Task</button>
+  <div v-if="taskList.length === 0">
+    <h3>No Task to display</h3>
+  </div>
+  <div v-else>
+    <ul>
+      <li v-for="(task, index) in taskList" :key="index">{{ task }}</li>
+    </ul>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<script setup>
+import { ref } from "vue";
+
+const taskList = ref([]);
+
+const currentTask = ref('');
+
+const addTask = () => {
+  taskList.value.push(currentTask.value);
+  currentTask.value = '';
+};
+</script>
+
+<style scoped></style>
+
